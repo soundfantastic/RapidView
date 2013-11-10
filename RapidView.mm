9c8fdc8702c2bad92fe8198c1448226e7ee59af9
Dragan Petrovic <polytonicdev@gmail.com>
2013-11-10 20:20:48 +0100

    superDrawRect function

--- RapidView.mm
+++ RapidView.mm
@@ -22,16 +22,19 @@
 #endif
 
 static int32_t _THIS_VIEW_COUNT = 0;
-//struct objc_super superView = { _THIS_VIEW_, _THIS_SUPERCLASS };
-//objc_msgSendSuper(&superView, @selector(drawRect:));
+
+void superDrawRect(Class _THIS_VIEW_, Class _THIS_SUPERCLASS) {
+    struct objc_super superView = { _THIS_VIEW_, _THIS_SUPERCLASS };
+    objc_msgSendSuper(&superView, @selector(drawRect:));
+}
 
 id drawWithMethod(SEL selector, id target, CGRect frame) {
     const char* name = [[NSString stringWithFormat:@"_VIEW_WITH_METHOD_%d", ++_THIS_VIEW_COUNT] UTF8String];
     Class _THIS_SUPERCLASS = NSClassFromString(_THIS_SUPERVIEW_STRING);
     Class _THIS_VIEW_ = objc_allocateClassPair(_THIS_SUPERCLASS, name, 0);
-    class_addMethod(_THIS_VIEW_, NSSelectorFromString(@"drawRect:"), imp_implementationWithBlock(^(id _self, CGRect dirtyRect) {
+    class_addMethod(_THIS_VIEW_, NSSelectorFromString(@"drawRect:"), imp_implementationWithBlock(^(id sender, CGRect dirtyRect) {
         if(class_respondsToSelector(object_getClass(target), selector)) {
-            objc_msgSend(target, selector, _self, _THIS_CONTEXT);
+            objc_msgSend(target, selector, sender, _THIS_CONTEXT);
         }
     }), [[NSString stringWithFormat:@"v:%s", @encode(_THIS_RECT)] UTF8String]);
     return [[_THIS_VIEW_ alloc] initWithFrame:frame]; //dont forget to release this object
@@ -41,10 +44,11 @@ id drawWithBlock(Draw block, CGRect frame) {
     const char* name = [[NSString stringWithFormat:@"_VIEW_WITH_BLOCK_%d", ++_THIS_VIEW_COUNT] UTF8String];
     Class _THIS_SUPERCLASS = NSClassFromString(_THIS_SUPERVIEW_STRING);
     Class _THIS_VIEW_ = objc_allocateClassPair(_THIS_SUPERCLASS, name, 0);
-    class_addMethod(_THIS_VIEW_, NSSelectorFromString(@"drawRect:"), imp_implementationWithBlock(^(id _self, CGRect dirtyRect) {
+    class_addMethod(_THIS_VIEW_, NSSelectorFromString(@"drawRect:"), imp_implementationWithBlock(^(id sender, CGRect dirtyRect) {
         if(block) {
-            block(_self, _THIS_CONTEXT);
+            block(sender, _THIS_CONTEXT);
         }
     }), [[NSString stringWithFormat:@"v:%s", @encode(_THIS_RECT)] UTF8String]);
     return [[_THIS_VIEW_ alloc] initWithFrame:frame]; //dont forget to release this object
 }
+
